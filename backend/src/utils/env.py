ee484f34c6a207642ba6a6105e4acc22c5e79b71
Your Name <you@example.com>
2025-09-07 01:51:37 +0800

    v

--- backend/src/utils/env.py
+++ backend/src/utils/env.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+
+
 def load_env():
     environment = os.getenv("ENVIRONMENT")
     if not environment:
@@ -8,11 +10,16 @@ def load_env():
     current_file = Path(__file__).resolve()
     parent_two = current_file.parent.parent
     dotenv_file_name = f".env.{environment}"
-    dotenv_file_path = parent_two/dotenv_file_name
+    dotenv_file_path = parent_two / dotenv_file_name
+    print(dotenv_file_path)
+    if not dotenv_file_path.exists():
+        raise Exception("env file not exist")
     load_dotenv(dotenv_path=dotenv_file_path)
     print(f"current environment : {environment}")
 
+
 def is_dev_environment():
     from src.setting import get_settings
+
     setting = get_settings()
     return setting.environment == "dev"
