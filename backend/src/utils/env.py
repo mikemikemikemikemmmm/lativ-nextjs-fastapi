0b70e1875c45c9efe06ea68f13c8b84080e663d4
Your Name <you@example.com>
2025-09-07 01:49:49 +0800

    v

--- backend/src/utils/env.py
+++ backend/src/utils/env.py
@@ -1,10 +1,14 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 def load_env():
     environment = os.getenv("ENVIRONMENT")
     if not environment:
         raise Exception("no environment env")
-    dotenv_file_path = f".env.{environment}"
+    current_file = Path(__file__).resolve()
+    parent_two = current_file.parent.parent
+    dotenv_file_name = f".env.{environment}"
+    dotenv_file_path = parent_two/dotenv_file_name
     load_dotenv(dotenv_path=dotenv_file_path)
     print(f"current environment : {environment}")
 
