a1ded0801bf125ed0113b31faff971c4b62c1358
Your Name <you@example.com>
2025-09-07 17:03:03 +0800

    5

--- backend/scripts/prod/run.sh
+++ backend/scripts/prod/run.sh
@@ -22,15 +22,30 @@ if [ ! -f /etc/systemd/system/$BACKEND_SERVICE_NAME.service ]; then
     sudo systemctl enable $BACKEND_SERVICE_NAME
 fi
 sudo systemctl restart $BACKEND_SERVICE_NAME
+if systemctl is-active --quiet "$BACKEND_SERVICE_NAME"; then
+    echo "$BACKEND_SERVICE_NAME 服務啟動成功 ✅"
+else
+    echo "$BACKEND_SERVICE_NAME 服務啟動失敗 ❌"
+    # 可以選擇退出部署
+    exit 1
+fi
 
 echo "啟動backend service成功"
-#啟動backend service
+#啟動MONITOR_SERVICE_NAME
 if [ ! -f /etc/systemd/system/$MONITOR_SERVICE_NAME.service ]; then
     sudo cp ./deploy/$MONITOR_SERVICE_NAME.service /etc/systemd/system/$MONITOR_SERVICE_NAME.service
     sudo systemctl daemon-reload
     sudo systemctl enable $MONITOR_SERVICE_NAME
 fi
 sudo systemctl restart $MONITOR_SERVICE_NAME
+if systemctl is-active --quiet "$MONITOR_SERVICE_NAME"; then
+    echo "$MONITOR_SERVICE_NAME 服務啟動成功 ✅"
+else
+    echo "$MONITOR_SERVICE_NAME 服務啟動失敗 ❌"
+    # 可以選擇退出部署
+    exit 1
+fi
+
 echo "啟動monitor service成功"
 
 echo "部署成功"
