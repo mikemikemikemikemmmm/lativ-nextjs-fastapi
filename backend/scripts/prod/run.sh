92c4828757d32ab26278d3dd86099d3ea3cc101d
Your Name <you@example.com>
2025-09-07 17:12:16 +0800

    5

--- backend/scripts/prod/run.sh
+++ backend/scripts/prod/run.sh
@@ -4,17 +4,17 @@ set -e  # 發生錯誤時停止
 export ENVIRONMENT=prod
 BACKEND_SERVICE_NAME=backend
 MONITOR_SERVICE_NAME=monitor
+cd $BACKEND_DIR 
+echo "移動到$BACKEND_DIR"
+
 # 生成 .env 文件
 echo 
-echo '${{ secrets.ENV_FILE_CONTENT }}' > ./.env.$ENVIRONMENT
+echo "${{ secrets.ENV_FILE_CONTENT }}"  > ./.env.$ENVIRONMENT
+echo "生成.env"
 
 uv --version || echo "cannot run uv"
 uv sync 
-
 echo "uv sync成功"
-cd $BACKEND_DIR 
-
-echo "移動到$BACKEND_DIR"
 #啟動backend service
 if [ ! -f /etc/systemd/system/$BACKEND_SERVICE_NAME.service ]; then
     sudo cp ./deploy/$BACKEND_SERVICE_NAME.service /etc/systemd/system/$BACKEND_SERVICE_NAME.service
