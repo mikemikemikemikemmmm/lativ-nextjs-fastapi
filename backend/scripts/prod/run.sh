876d6e92ead2e7ee74d215ecfe3caab6aef8b882
Your Name <you@example.com>
2025-09-07 16:51:03 +0800

    a

--- backend/scripts/prod/run.sh
+++ backend/scripts/prod/run.sh
@@ -5,11 +5,13 @@ export ENVIRONMENT=prod
 BACKEND_SERVICE_NAME=backend
 MONITOR_SERVICE_NAME=monitor
 # 生成 .env 文件
-echo '${{ secrets.ENV_FILE_CONTENT }}' > ./.env.prod
+echo 
+echo '${{ secrets.ENV_FILE_CONTENT }}' > ./.env.$ENVIRONMENT
 
 uv --version || echo "cannot run uv"
 uv sync 
 
+echo "uv sync成功"
 #啟動backend service
 if [ ! -f /etc/systemd/system/$BACKEND_SERVICE_NAME.service ]; then
     sudo cp $BACKEND_SERVICE_NAME/deploy/$BACKEND_SERVICE_NAME.service /etc/systemd/system/$BACKEND_SERVICE_NAME.service
@@ -18,6 +20,7 @@ if [ ! -f /etc/systemd/system/$BACKEND_SERVICE_NAME.service ]; then
 fi
 sudo systemctl restart $BACKEND_SERVICE_NAME
 
+echo "啟動backend service成功"
 #啟動backend service
 if [ ! -f /etc/systemd/system/$MONITOR_SERVICE_NAME.service ]; then
     sudo cp $MONITOR_SERVICE_NAME/deploy/$MONITOR_SERVICE_NAME.service /etc/systemd/system/$MONITOR_SERVICE_NAME.service
@@ -25,6 +28,6 @@ if [ ! -f /etc/systemd/system/$MONITOR_SERVICE_NAME.service ]; then
     sudo systemctl enable $MONITOR_SERVICE_NAME
 fi
 sudo systemctl restart $MONITOR_SERVICE_NAME
+echo "啟動monitor service成功"
 
-
-echo "Deployment completed successfully"
+echo "部署成功"
