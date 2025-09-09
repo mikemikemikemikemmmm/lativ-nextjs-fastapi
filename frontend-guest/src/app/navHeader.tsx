ada9cfb5205b48182812e1d1bd149236c00c8a03
Your Name <you@example.com>
2025-09-09 16:55:31 +0800

    v

--- frontend-guest/src/app/navHeader.tsx
+++ frontend-guest/src/app/navHeader.tsx
@@ -5,27 +5,27 @@ import { NavRead } from "@/types"
 import Link from "next/link"
 export const NavHeader = () => {
     const [_, navs] = useGetData<NavRead>("navs")
-    if (navs === "loading") {
-        return null
-    }
-    return <section className="my-4 h-40">
-        <div className="text-right"></div>
-        <div className="flex px-2">
-            <Link className="inline-flex justify-center items-end text-center text-4xl font-extrabold text-nav-header-logo hover:cursor-pointer" href={"/"} style={{ width: ASIDE_WIDTH }}>
-                lativ
-            </Link>
-            <div className="inline-flex flex-1 items-end">
-                {
-                    navs.map(n => <Link
-                        key={n.id}
-                        href={`/home/${n.route}`}
-                        style={{ width: 100 }}
-                        className={` text-center mx-4 text-nav-header-item hover:bg-nav-header-item-bg hover:text-nav-header-item-hover`}
-                    >
-                        {n.name}
-                    </Link>)
-                }
+    return <section className="my-4 h-[40px]">
+        {navs === "loading" ? null : <>
+            <div className="text-right"></div>
+            <div className="flex px-2">
+                <Link className="inline-flex justify-center items-end text-center text-4xl font-extrabold text-nav-header-logo hover:cursor-pointer" href={"/"} style={{ width: ASIDE_WIDTH }}>
+                    lativ
+                </Link>
+                <div className="inline-flex flex-1 items-end">
+                    {
+                        navs.map(n => <Link
+                            key={n.id}
+                            href={`/home/${n.route}`}
+                            style={{ width: 100 }}
+                            className={` text-center mx-4 text-nav-header-item hover:bg-nav-header-item-bg hover:text-nav-header-item-hover`}
+                        >
+                            {n.name}
+                        </Link>)
+                    }
+                </div>
             </div>
-        </div>
+        </>
+        }
     </section>
 }
