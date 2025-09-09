93ef8ccddaa549f5c4681063f6daf97d9fcfaacc
Your Name <you@example.com>
2025-09-09 16:12:39 +0800

    v

--- backend/scripts/prod/monitor.py
+++ backend/scripts/prod/monitor.py
@@ -41,25 +41,25 @@ if "" in [EMAIL_FROM, EMAIL_PASSWORD, EMAIL_TO, MONITOR_CHECK_URL,LOG_DIR]:
     raise EnvironmentError("環境變數有空值")
 
 # ---------- 日誌設定 ----------
-logger = logging.getLogger("monitor")
+# logger = logging.getLogger("monitor")
 
-def setup_log(console_level=logging.INFO, file_level=logging.DEBUG, max_bytes=10*1024*1024, backup_count=5):
-    """設定 logger"""
-    logger.setLevel(logging.DEBUG)  # 設定 logger 最低等級為 DEBUG，handler 可過濾
+# def setup_log(console_level=logging.INFO, file_level=logging.DEBUG, max_bytes=10*1024*1024, backup_count=5):
+#     """設定 logger"""
+#     logger.setLevel(logging.DEBUG)  # 設定 logger 最低等級為 DEBUG，handler 可過濾
 
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+#     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-    # 避免重複加入 handler
-    if not logger.hasHandlers():
-        os.makedirs(LOG_DIR, exist_ok=True)
-        log_file_path = os.path.join(LOG_DIR, "monitor.log")
+#     # 避免重複加入 handler
+#     if not logger.hasHandlers():
+#         os.makedirs(LOG_DIR, exist_ok=True)
+#         log_file_path = os.path.join(LOG_DIR, "monitor.log")
         
-        file_handler = RotatingFileHandler(
-            log_file_path, maxBytes=max_bytes, backupCount=backup_count, encoding="utf-8"
-        )
-        file_handler.setLevel(file_level)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+#         file_handler = RotatingFileHandler(
+#             log_file_path, maxBytes=max_bytes, backupCount=backup_count, encoding="utf-8"
+#         )
+#         file_handler.setLevel(file_level)
+#         file_handler.setFormatter(formatter)
+#         logger.addHandler(file_handler)
 
 
 # ---------- 郵件發送 ----------
