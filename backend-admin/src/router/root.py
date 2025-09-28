85186813a1c26283c049be7c12f5cdb56e3745a5
Your Name <you@example.com>
2025-09-28 18:53:24 +0800

    4

--- backend-admin/src/router/root.py
+++ backend-admin/src/router/root.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from .v1.root import v1_root_router
-
-root_router = APIRouter()
+from src.utils.env import is_dev_environment
+root_router = APIRouter(prefix="/" if is_dev_environment else "/admin")
 
 root_router.include_router(v1_root_router, prefix="/v1")
 @root_router.get("/health_check")
