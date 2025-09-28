85da5b1a8eacd6d42e1c2db6085c482e306f99ce
Your Name <you@example.com>
2025-09-28 17:15:12 +0800

    v

--- backend-admin/src/router/root.py
+++ backend-admin/src/router/root.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from .v1.root import v1_root_router
 
 root_router = APIRouter()
+
 root_router.include_router(v1_root_router, prefix="/v1")
 @root_router.get("/health_check")
 def get_health():
