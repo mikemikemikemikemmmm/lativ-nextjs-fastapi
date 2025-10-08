ba17dfdd669f8d5745c35bb3079a532e831dc752
Your Name <you@example.com>
2025-10-08 17:12:01 +0800

    c

--- frontend-guest-2/src/app/notFound.tsx
+++ frontend-guest-2/src/app/notFound.tsx
@@ -3,6 +3,6 @@ import { ENV } from "@/utils/env";
 
 export const NotFoundUI = () => {
     return <div className="flex justify-center items-center w-dvw h-dvh">
-        <Image src={`${ENV.baseUrl}/404.jpg`} height={250} width={400} alt="404" />
+        <Image src={`/404.jpg`} height={250} width={400} alt="404" />
     </div>
 }
