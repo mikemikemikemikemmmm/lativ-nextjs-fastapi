c5197e1c5f55d34f16216bff43bf63f0d228c0d9
Your Name <you@example.com>
2025-10-08 15:49:04 +0800

    frontend pages

--- frontend-guest-2/vite.config.ts
+++ frontend-guest-2/vite.config.ts
@@ -3,14 +3,22 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const common = {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  }, server: {
-    port: 3001, // 👈 你想要的 port，例如 5175
-    open: true, // 啟動時自動開啟瀏覽器（可選）
-  },
+    server: {
+      port: 3001, // 👈 你想要的 port，例如 5175
+      open: true, // 啟動時自動開啟瀏覽器（可選）
+    },
+  }
+  if (mode === "production") {
+    return { ...common, base: "/lativ-nextjs-fastapi/" }
+  } else {
+    return common
+  }
 })
